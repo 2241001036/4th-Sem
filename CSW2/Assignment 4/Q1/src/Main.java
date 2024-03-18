7a356af8e89b743b108472ff18714a043d4e1508
Aryan <140298288+2241001036@users.noreply.github.com>
2024-03-18 16:18:46 +0530

    Add files via upload

--- CSW2/Assignment 4/Q1/src/Main.java
+++ CSW2/Assignment 4/Q1/src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Pair<String, Integer> pair = new Pair<>("Age", 25);
+        String key = pair.getKey();
+        Integer value = pair.getValue();
+        System.out.println("Key: " + key + ", Value: " + value);
+        System.out.println("Pair: " + pair);
     }
 }
+
